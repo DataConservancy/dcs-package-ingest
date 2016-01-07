af36656b77185597e1e0e36cb254f267bec02e31
Aaron Birkland <apb@jhu.edu>
2016-01-07 12:17:10 -0500

    DC-2264:  Initial camel integration tests

---
+++ package-ingest-integration/src/test/java/org/dataconservancy/packaging/ingest/integration/FedoraTestConstants.java
@@ -0,0 +1,6 @@
+package org.dataconservancy.packaging.ingest.integration;
+
+public interface FedoraTestConstants {
+	public static final String FEDORA_BASEURI = System.getProperty(
+			"fedora.baseURI", "http://localhost:8080/fcrepo/rest");
+}
