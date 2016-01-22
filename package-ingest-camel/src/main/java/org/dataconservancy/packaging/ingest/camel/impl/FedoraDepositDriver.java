e9eca7c27bbfb7ae37917ef09bad094422ef820a
Aaron Birkland <apb@jhu.edu>
2016-01-22 12:54:06 -0500

    DC-2268: Add Fedora transaction support

--- package-ingest-camel/src/main/java/org/dataconservancy/packaging/ingest/camel/impl/FedoraDepositDriver.java
+++ package-ingest-camel/src/main/java/org/dataconservancy/packaging/ingest/camel/impl/FedoraDepositDriver.java
@@ -1,12 +1,122 @@
 
 package org.dataconservancy.packaging.ingest.camel.impl;
 
+import java.util.Map;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+import org.apache.http.HttpHeaders;
+
+import org.dataconservancy.packaging.ingest.camel.DepositDriver;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+
+@Component(service = DepositDriver.class, configurationPolicy = ConfigurationPolicy.REQUIRE)
 public class FedoraDepositDriver
         extends LdpDepositDriver {
+
+    static final String HEADER_FCTRPO_TX_BASEURI = "txBaseURI";
+
+    static final String PROP_FEDORA_BASEURI = "fcrepo.baseuri";
+
+    static final String ID_START_TRANSACTION = "fedora-start-transaction";
+
+    static final String ID_COMMIT_TRANSACTION = "fedora-commit-transaction";
+
+    static final String ID_ROLLBACK_TRANSACTION = "fedora-rollback-transaction";
+
+    private String fedoraBaseURI;
+
+    @Activate
+    public void init(Map<String, String> config) {
+        super.init(config);
+        fedoraBaseURI =
+                config.getOrDefault(PROP_FEDORA_BASEURI,
+                                    "http://localhost:8080/fedora/rest");
+    }
 
     @Override
     protected void configureTransactions() {
         /* TODO */
+        from(ROUTE_TRANSACTION_BEGIN).enrich("direct:_doStartTransaction",
+                                             TX_DETAILS);
+
+        from(ROUTE_TRANSACTION_COMMIT).enrich("direct:_doCommitTransaction",
+                                              ((orig, resp) -> orig));
+
+        from(ROUTE_TRANSACTION_ROLLBACK)
+                .enrich("direct:_doRollbackTransaction", ((orig, resp) -> orig));
+
+        from(ROUTE_TRANSACTION_CANONICALIZE)
+                .process(e -> {
+                    e.getIn()
+                            .setHeader(Exchange.HTTP_URI,
+                                       headerString(e, Exchange.HTTP_URI)
+                                               .replace(headerString(e,
+                                                                     HEADER_FCTRPO_TX_BASEURI),
+                                                        fedoraBaseURI));
+                });
+
+        /*
+         * Actually does the work of starting a transaction in Fedora
+         * via HTTP, sends a POST to the fcr:tx endpoint, which creates
+         * a transaction in Fedora.
+         */
+        from("direct:_doStartTransaction")
+                .id(ID_START_TRANSACTION)
+                .removeHeaders("*", fedoraBaseURI)
+                .setHeader(Exchange.HTTP_URI,
+                           constant(fedoraBaseURI + "/fcr:tx"))
+                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+                .to("http4:fcrepo-host");
+
+        /*
+         * Actually does the work of committing a trancation in Fedora
+         * via HTTP, sends a POST to the local transaction's
+         * fcr:tx/fcr:commit endpoint.
+         */
+        from("direct:_doCommitTransaction")
+                .id(ID_COMMIT_TRANSACTION)
+                .removeHeaders("*", HEADER_FCTRPO_TX_BASEURI)
+                .setHeader(Exchange.HTTP_URI,
+                           simple(String
+                                   .format("${in.header.%s}/fcr:tx/fcr:commit",
+                                           HEADER_FCTRPO_TX_BASEURI)))
+                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+                .to("http4:fcrepo-host");
+
+        /*
+         * Actually does the work of transaction rollback in Fedora via
+         * HTTP, sends a POST to the local transaction's
+         * fcr:tx/fcr:rollback endpoint.
+         */
+        from("direct:_doRollbackTransaction")
+                .id(ID_ROLLBACK_TRANSACTION)
+                .removeHeaders("*", HEADER_FCTRPO_TX_BASEURI)
+                .setHeader(Exchange.HTTP_URI,
+                           simple(String
+                                   .format("${in.header.%s}/fcr:tx/fcr:rollback",
+                                           HEADER_FCTRPO_TX_BASEURI)))
+                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+                .to("http4:fcrepo-host");
+
+    }
+
+    AggregationStrategy TX_DETAILS = (orig, tx) -> {
+        String txBase = headerString(tx, HttpHeaders.LOCATION);
+
+        String dest = headerString(orig, Exchange.HTTP_URI);
+
+        orig.getIn().setHeader(HEADER_FCTRPO_TX_BASEURI, txBase);
+
+        orig.getIn().setHeader(Exchange.HTTP_URI,
+                               dest.replace(fedoraBaseURI, txBase));
+        return orig;
+    };
+
+    static String headerString(Exchange e, String name) {
+        return e.getIn().getHeader(name, String.class);
     }
 
 }
---
+++ package-ingest-camel/src/test/java/org/dataconservancy/packaging/ingest/camel/impl/FedoraDepositDriverTest.java
@@ -0,0 +1,251 @@
+
+package org.dataconservancy.packaging.ingest.camel.impl;
+
+import java.util.HashMap;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.http.HttpHeaders;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.dataconservancy.packaging.ingest.camel.DepositDriver.ROUTE_TRANSACTION_BEGIN;
+import static org.dataconservancy.packaging.ingest.camel.DepositDriver.ROUTE_TRANSACTION_CANONICALIZE;
+import static org.dataconservancy.packaging.ingest.camel.DepositDriver.ROUTE_TRANSACTION_COMMIT;
+import static org.dataconservancy.packaging.ingest.camel.DepositDriver.ROUTE_TRANSACTION_ROLLBACK;
+import static org.dataconservancy.packaging.ingest.camel.impl.FedoraDepositDriver.HEADER_FCTRPO_TX_BASEURI;
+import static org.dataconservancy.packaging.ingest.camel.impl.FedoraDepositDriver.ID_COMMIT_TRANSACTION;
+import static org.dataconservancy.packaging.ingest.camel.impl.FedoraDepositDriver.ID_ROLLBACK_TRANSACTION;
+import static org.dataconservancy.packaging.ingest.camel.impl.FedoraDepositDriver.ID_START_TRANSACTION;
+import static org.dataconservancy.packaging.ingest.camel.impl.FedoraDepositDriver.PROP_FEDORA_BASEURI;
+
+@SuppressWarnings("serial")
+public class FedoraDepositDriverTest
+        extends CamelTestSupport {
+
+    static final String FEDORA_BASEURI = "http://example.org/fedora";
+
+    static final String FEDORA_TX_BASEURI = "http://example.org/fedora/tx/123";
+
+    static final String OPERATION_TX_COMMIT = "test.txcommit";
+
+    static final String OPERATION_TX_ROLLBACK = "test.txrollback";
+
+    String operation;
+
+    FedoraDepositDriver driver;
+
+    @EndpointInject(uri = "mock:out")
+    private MockEndpoint mockOut = new MockEndpoint();
+
+    @Produce
+    private ProducerTemplate template;
+
+    @Before
+    public void clear() {
+        this.operation = null;
+    }
+
+    @Test
+    public void transactionBeginTest() throws Exception {
+        context.getRouteDefinition(ID_START_TRANSACTION)
+                .adviceWith(context, new FakeTX());
+
+        String PATH = "/test/path";
+        String ORIG_URI = FEDORA_BASEURI + PATH;
+        String TEST_BODY = "test.body";
+        String TEST_HEADER = "test.header";
+        String TEST_HEADER_VALUE = "test.header.value";
+
+        template.sendBodyAndHeaders("direct:testBeginTransaction",
+                                    TEST_BODY,
+                                    new HashMap<String, Object>() {
+
+                                        {
+                                            put(Exchange.HTTP_URI, ORIG_URI);
+                                            put(TEST_HEADER, TEST_HEADER_VALUE);
+
+                                        }
+                                    });
+
+        mockOut.setExpectedCount(1);
+
+        assertMockEndpointsSatisfied();
+
+        Message msg = mockOut.getExchanges().get(0).getIn();
+        assertEquals(FEDORA_TX_BASEURI + PATH, msg.getHeader(Exchange.HTTP_URI));
+        assertEquals(TEST_BODY, msg.getBody());
+        assertEquals(TEST_HEADER_VALUE, msg.getHeader(TEST_HEADER));
+        assertEquals(FEDORA_TX_BASEURI, msg.getHeader(HEADER_FCTRPO_TX_BASEURI));
+    }
+
+    @Test
+    public void transactionCommitTest() throws Exception {
+        context.getRouteDefinition(ID_COMMIT_TRANSACTION)
+                .adviceWith(context, new FakeTX());
+
+        String PATH = "/test/path";
+        String ORIG_URI = FEDORA_TX_BASEURI + PATH;
+        String TEST_BODY = "test.body";
+        String TEST_HEADER = "test.header";
+        String TEST_HEADER_VALUE = "test.header.value";
+
+        template.sendBodyAndHeaders("direct:testCommitTransaction",
+                                    TEST_BODY,
+                                    new HashMap<String, Object>() {
+
+                                        {
+                                            put(Exchange.HTTP_URI, ORIG_URI);
+                                            put(TEST_HEADER, TEST_HEADER_VALUE);
+                                            put(HEADER_FCTRPO_TX_BASEURI,
+                                                FEDORA_TX_BASEURI);
+                                        }
+                                    });
+
+        mockOut.setExpectedCount(1);
+
+        assertMockEndpointsSatisfied();
+
+        Message msg = mockOut.getExchanges().get(0).getIn();
+        assertEquals(TEST_BODY, msg.getBody());
+        assertEquals(TEST_HEADER_VALUE, msg.getHeader(TEST_HEADER));
+        assertEquals(OPERATION_TX_COMMIT, operation);
+    }
+
+    @Test
+    public void transactionRollbackTest() throws Exception {
+        context.getRouteDefinition(ID_ROLLBACK_TRANSACTION)
+                .adviceWith(context, new FakeTX());
+
+        String PATH = "/test/path";
+        String ORIG_URI = FEDORA_TX_BASEURI + PATH;
+        String TEST_BODY = "test.body";
+        String TEST_HEADER = "test.header";
+        String TEST_HEADER_VALUE = "test.header.value";
+
+        template.sendBodyAndHeaders("direct:testRollbackTransaction",
+                                    TEST_BODY,
+                                    new HashMap<String, Object>() {
+
+                                        {
+                                            put(Exchange.HTTP_URI, ORIG_URI);
+                                            put(TEST_HEADER, TEST_HEADER_VALUE);
+                                            put(HEADER_FCTRPO_TX_BASEURI,
+                                                FEDORA_TX_BASEURI);
+                                        }
+                                    });
+
+        mockOut.setExpectedCount(1);
+
+        assertMockEndpointsSatisfied();
+
+        Message msg = mockOut.getExchanges().get(0).getIn();
+        assertEquals(TEST_BODY, msg.getBody());
+        assertEquals(TEST_HEADER_VALUE, msg.getHeader(TEST_HEADER));
+        assertEquals(OPERATION_TX_ROLLBACK, operation);
+    }
+
+    @Test
+    public void canonicalizeTest() throws Exception {
+
+        String PATH = "/test/path";
+        String ORIG_URI = FEDORA_TX_BASEURI + PATH;
+        String TEST_BODY = "test.body";
+        String TEST_HEADER = "test.header";
+        String TEST_HEADER_VALUE = "test.header.value";
+
+        template.sendBodyAndHeaders("direct:testCanonicalize",
+                                    TEST_BODY,
+                                    new HashMap<String, Object>() {
+
+                                        {
+                                            put(Exchange.HTTP_URI, ORIG_URI);
+                                            put(TEST_HEADER, TEST_HEADER_VALUE);
+                                            put(HEADER_FCTRPO_TX_BASEURI,
+                                                FEDORA_TX_BASEURI);
+                                        }
+                                    });
+
+        mockOut.setExpectedCount(1);
+
+        assertMockEndpointsSatisfied();
+
+        Message msg = mockOut.getExchanges().get(0).getIn();
+        assertEquals(TEST_BODY, msg.getBody());
+        assertEquals(TEST_HEADER_VALUE, msg.getHeader(TEST_HEADER));
+        assertEquals(FEDORA_BASEURI + PATH, msg.getHeader(Exchange.HTTP_URI));
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        driver = new FedoraDepositDriver();
+        driver.init(new HashMap<String, String>() {
+
+            {
+                put(PROP_FEDORA_BASEURI, FEDORA_BASEURI);
+            }
+        });
+
+        return new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+                driver.addRoutesToCamelContext(context());
+
+                from("direct:testBeginTransaction").to(ROUTE_TRANSACTION_BEGIN)
+                        .to("mock:out");
+
+                from("direct:testCommitTransaction")
+                        .to(ROUTE_TRANSACTION_COMMIT).to("mock:out");
+
+                from("direct:testRollbackTransaction")
+                        .to(ROUTE_TRANSACTION_ROLLBACK).to("mock:out");
+
+                from("direct:testCanonicalize")
+                        .to(ROUTE_TRANSACTION_CANONICALIZE).to("mock:out");
+            }
+        };
+    }
+
+    private class FakeTX
+            extends AdviceWithRouteBuilder {
+
+        @Override
+        public void configure() throws Exception {
+            interceptSendToEndpoint("http4:fcrepo-host")
+                    .skipSendToOriginalEndpoint()
+                    .choice()
+                    .when(header(Exchange.HTTP_URI).isEqualTo(FEDORA_BASEURI
+                            + "/fcr:tx"))
+                    .process(e -> assertEquals("POST",
+                                               e.getIn()
+                                                       .getHeader(Exchange.HTTP_METHOD)))
+                    .removeHeaders("*")
+                    .setHeader(HttpHeaders.LOCATION,
+                               constant(FEDORA_TX_BASEURI))
+                    .when(header(Exchange.HTTP_URI).isEqualTo(FEDORA_TX_BASEURI
+                            + "/fcr:tx/fcr:commit"))
+                    .process(e -> {
+                        assertEquals("POST",
+                                     e.getIn().getHeader(Exchange.HTTP_METHOD));
+                        operation = OPERATION_TX_COMMIT;
+                    })
+                    .when(header(Exchange.HTTP_URI).isEqualTo(FEDORA_TX_BASEURI
+                            + "/fcr:tx/fcr:rollback"))
+                    .process(e -> {
+                        assertEquals("POST",
+                                     e.getIn().getHeader(Exchange.HTTP_METHOD));
+                        operation = OPERATION_TX_ROLLBACK;
+                    });
+
+        }
+    };
+}
