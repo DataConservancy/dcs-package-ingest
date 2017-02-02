f58bc69100c72996f58533ce4eb06288e49455c3
Aaron Birkland <apb@jhu.edu>
2017-02-02 13:59:38 -0500

    Fix licenses

--- package-ingest-impl/src/test/java/org/dataconservancy/packaging/impl/deposit/SingleDepositManagerTest.java
+++ package-ingest-impl/src/test/java/org/dataconservancy/packaging/impl/deposit/SingleDepositManagerTest.java
@@ -1,11 +1,9 @@
 /*
- * Licensed to DuraSpace under one or more contributor license agreements.
- * See the NOTICE file distributed with this work for additional information
- * regarding copyright ownership.
+ * Copyright 2017 Johns Hopkins University
  *
- * DuraSpace licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except in
- * compliance with the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -36,12 +34,12 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 
+import org.dataconservancy.packaging.ingest.DepositFactory;
 import org.dataconservancy.packaging.ingest.DepositNotifier;
 import org.dataconservancy.packaging.ingest.Depositor;
-import org.dataconservancy.packaging.ingest.DepositFactory;
-import org.dataconservancy.packaging.ingest.PackagedResource;
 import org.dataconservancy.packaging.ingest.PackageWalker;
 import org.dataconservancy.packaging.ingest.PackageWalkerFactory;
+import org.dataconservancy.packaging.ingest.PackagedResource;
 import org.dataconservancy.packaging.ingest.PackagedResource.Type;
 
 import org.junit.Before;
